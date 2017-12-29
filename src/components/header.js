4090203fffcb607fca29b3a94d67b958022f0922
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-29 15:32:24 -0500

    Added more advanced styling to view component of header

--- src/components/header.js
+++ src/components/header.js
@@ -14,7 +14,14 @@ export default Header;
 
 const styles = {
   viewStyle: {
-    backgroundColor: '#E8E8E8'
+    backgroundColor: '#E8E8E8',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 60,
+    paddingTop: 15, // To go under the ios status bar
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.4
   },
   textStyle: {
     fontSize: 20

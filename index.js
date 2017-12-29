e0223a2631a8c9febe99f09c5e096988abfe323d
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-29 15:47:03 -0500

    Added dummy AlbumList component

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 import React from 'react';
-import { AppRegistry } from 'react-native';
+import { View, AppRegistry } from 'react-native';
 import Header from './src/components/header';
+import AlbumList from './src/components/albumlist';
 
 const App = () => (
-  <Header title="Albums" />
+  <View>
+    <Header title="Albums" />
+    <AlbumList />
+  </View>
 );
 
 AppRegistry.registerComponent('udemy_react_native_course', () => App);
---
+++ src/components/albumlist.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const AlbumList = () => {
+  return (
+    <View>
+      <Text>Album List (Under construction)</Text>
+    </View>
+  );
+};
+
+export default AlbumList;

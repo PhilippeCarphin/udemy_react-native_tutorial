1cf44c52c10ef5c01332c2f94745f611eb978b0f
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-29 15:49:05 -0500

    Switched AlbumList to class based component

--- src/components/albumlist.js
+++ src/components/albumlist.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Text, View } from 'react-native';
 
-const AlbumList = () => {
-  return (
-    <View>
-      <Text>Album List (Under construction)</Text>
-    </View>
-  );
-};
+class AlbumList extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Album List (Under construction)</Text>
+      </View>
+    );
+  }
+}
 
 export default AlbumList;

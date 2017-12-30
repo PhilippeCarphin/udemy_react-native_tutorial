0f61800440cc4d1265219d930f590068b2266058
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-30 18:14:45 -0500

    Debugging output for componentWillMoutn

--- src/components/albumlist.js
+++ src/components/albumlist.js
@@ -5,10 +5,13 @@ import axios from 'axios';
 class AlbumList extends Component {
   state = { albums: [] };
   componentWillMount() {
+	  console.log("componentWillMount() called");
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
+	  console.log("componentWillMount() finished");
   }
   render() {
+	  console.log("render() called");
     return (
       <View>
         <Text>Album List (Under construction)</Text>

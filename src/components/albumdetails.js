04bc5d881a26398f950a51e2c70370861ae23466
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-31 15:26:57 -0500

    Added styling to first cardsection of albumdetails

--- src/components/albumdetails.js
+++ src/components/albumdetails.js
@@ -5,18 +5,23 @@ import CardSection from './cardsection';
 
 const AlbumDetails = ({ album }) => {
   const { title, artist, thumbnail_image } = album;
-  const { thumbnailStyle, headerContentStyle } = styles;
+  const {
+    thumbnailStyle,
+    headerContentStyle,
+    titleTextStyle,
+    thumbnailContainerStyle
+  } = styles;
   return (
     <Card>
       <CardSection>
-        <View>
+        <View style={thumbnailContainerStyle}>
           <Image
             style={thumbnailStyle}
             source={{ uri: thumbnail_image }}
           />
         </View>
         <View style={headerContentStyle}>
-          <Text>{title}</Text>
+          <Text style={titleTextStyle}>{title}</Text>
           <Text>{artist}</Text>
         </View>
       </CardSection>
@@ -29,9 +34,18 @@ const styles = {
     flexDirection: 'column',
     justifyContent: 'space-around'
   },
+  titleTextStyle: {
+    fontSize: 18
+  },
   thumbnailStyle: {
     height: 50,
     width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10
   }
 };
 

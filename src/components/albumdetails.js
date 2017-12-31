681335cccb885d3742bede7c199d918ced916d2e
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-31 14:51:00 -0500

    Create and use CardSection component

--- src/components/albumdetails.js
+++ src/components/albumdetails.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import { Text } from 'react-native';
-import Card from './card'
+import Card from './card';
+import CardSection from './cardsection';
 
 const AlbumDetails = (props) => {
   return (
     <Card>
-      <Text>{props.album.title}</Text>
+      <CardSection>
+        <Text>{props.album.title}</Text>
+      </CardSection>
+      <Text>Not in CardSection</Text>
     </Card>
   );
 };

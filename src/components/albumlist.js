702e81b9692cdfd00c191273c46bbfaaeeec09cd
Philippe Carphin <philippe.carphin@polymtl.ca>
2017-12-31 14:12:02 -0500

    whitespace change

--- src/components/albumlist.js
+++ src/components/albumlist.js
@@ -4,10 +4,12 @@ import axios from 'axios';
 
 class AlbumList extends Component {
   state = { albums: [] };
+
   componentWillMount() {
     axios.get('https://rallycoding.herokuapp.com/api/music_albums')
       .then(response => this.setState({ albums: response.data }));
   }
+
   render() {
     return (
       <View>
